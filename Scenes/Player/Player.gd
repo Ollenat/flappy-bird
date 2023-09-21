3f4f4b54cd7e256a678defa04f08989e6bdca5bb
Realgymlinkopingmedia01 <145031690+Realgymlinkopingmedia01@users.noreply.github.com>
2023-09-21 17:58:20 +0200

    Added Keymapping for flapping and fixed rotation for bird

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const ROTATION_CONSTANT = 400.0
+const ROTATION_OFFSET = -90
 const JUMP_VELOCITY = -400.0
 
 @onready var animPlayer = get_node("AnimationPlayer") 
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,14 +13,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	velocity.y += gravity * delta
-	rotate(velocity.y * 0.0005)
+	rotation = atan2(ROTATION_CONSTANT, -velocity.y) + deg_to_rad(ROTATION_OFFSET) # negativt för att godot är konstigt
 	
 	if velocity.y > 0:
 		animPlayer.play("Fall")
 	
 	
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("flap"):
 		velocity.y = JUMP_VELOCITY
 		animPlayer.play("Flap")
 		
